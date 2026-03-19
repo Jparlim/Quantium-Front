dd2329d9a2b7f7640aa757fd464cf3bbf25ce407
Jparlim <gusjao0500@gmail.com>
2026-03-19 11:38:57 -0300

    built a function that will get data for gemini

--- front/src/pages/IA/index.tsx
+++ front/src/pages/IA/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ia from "../../../Images/Tool.png"
 import client from "../../../Images/Users.png"
 import financial from "../../../Images/Bar chart-2.png"
@@ -6,11 +6,43 @@ import calendar from "../../../Images/Calendar.png"
 import menu from "../../../Images/Menu.png"
 import home from "../../../Images/Home (1).png"
 import clipboard from "../../../Images/Clipboard.png"
+import pen from "../../../Images/Icon.png"
+import trash from "../../../Images/Icon (1).png"
+import axios from "axios";
+
+interface datasIA {
+    [nome:string]: {type: string | number | boolean}
+}
 
 export function IA() {
-
     const [isOpen, setIsOpen] = useState(false);
-    
+    const [list, setlist] = useState<datasIA>({});
+
+    useEffect(() => {
+        async function getDatasIA() {
+            try {
+                const data = await axios.get<datasIA>("http://localhost:3000/datas_IA")
+                console.log(data)
+                setlist(data.data)
+            } catch(error) {
+                console.log(error);
+            }
+        }
+
+        getDatasIA();
+    })
+
+    const handleDeleteANDPut = async function( nome:string, tipo: string | number | boolean, style: "Delete" | "Put" ) {
+        if(style === "Delete") {
+            await axios.put("http://localhost:3000/...", {
+
+            })
+        }
+
+        if(style === "Put") {
+            await axios.delete("http://localhost:3000/...")
+        }
+    }
 
     return (
         <div className="bg-backGround h-screen flex items-center justify-center relative">
@@ -127,23 +159,41 @@ export function IA() {
 
                             <div className="flex gap-2 justify-between">
                                 <p className="text-[17px] text-black/50 font-sans font-semibold">Instruções gerais</p>
-                                <input type="text" className="w-[70%] h-32 border-current-bordas border-[2px] pl-4 rounded-[5px] focus:outline-none"/>
+                                <textarea className="w-[70%] h-32 border-current-bordas border-[2px] pt-2 pl-4 rounded-[5px] focus:outline-none "/>
                             </div>
                         </div>
                     </div>
                     
-                    <div className="flex flex-col my-5">
+                    <div className="flex flex-col my-5 gap-4">
                         <h2 className="font-normal text-2xl">Dados que a IA deve coletar</h2>
                         
-                        <div className="w-[30%] h-auto border-[2px] rounded-md">
-                            <div className="flex justify-between mx-6">
+                        <div className="w-[40%] h-auto border-[2px] rounded-md">
+
+                            <div className="flex justify-between mx-2 px-4 pr-36 border-b-2">
                                 <p className="text-[17px] text-black/50 font-sans font-semibold">Nome do dado</p>
                                 <p className="text-[17px] text-black/50 font-sans font-semibold">Tipo</p>
                             </div>
-                        </div>
-                    </div>
-                </div>
 
+                            {Object.entries(list!).map(dado => {
+                                return (
+                                <div className="flex h-8 justify-between mx-2 px-6 border-b-2 items-center">
+                                    <p className="font-normal">{dado[0]}</p>
+                                    <p className="font-normal">{dado[1].type}</p>
+
+                                    <div className="flex gap-2 size-5">
+                                        <img className="hover:cursor-pointer" src={pen} alt="" onClick={() => handleDeleteANDPut(dado[0], dado[1].type, "Put")}/>
+                                        <img className="hover:cursor-pointer" src={trash} alt="" onClick={() => handleDeleteANDPut(dado[0], dado[1].type, "Delete")}/>
+                                    </div>
+                                </div>
+                            )})}
+                        
+                        </div>
+
+                    </div>
+                    <button>
+
+                    </button>
+                </div>
             </div>
 
         </div>
